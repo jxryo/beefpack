97abbb48587f6df2b2b3e0e6cc2d2a4239bc96f0
ryo <335868692@qq.com>
2020-01-09 16:20:35 +0800

    version 1 add upload

--- src/components/write_artical.vue
+++ src/components/write_artical.vue
@@ -3,52 +3,17 @@
   <div class="input_artical">
     <Form :model="formItem" :label-width="80">
       <FormItem label="title">
-        <Input v-model="formItem.input" placeholder="Enter something..."></Input>
+        <Input v-model="formItem.title" placeholder="Enter something..."/>
       </FormItem>
-<!--      <FormItem label="Select">-->
-<!--        <Select v-model="formItem.select">-->
-<!--          <Option value="beijing">New York</Option>-->
-<!--          <Option value="shanghai">London</Option>-->
-<!--          <Option value="shenzhen">Sydney</Option>-->
-<!--        </Select>-->
-<!--      </FormItem>-->
-<!--      <FormItem label="DatePicker">-->
-<!--        <Row>-->
-<!--          <Col span="11">-->
-<!--            <DatePicker type="date" placeholder="Select date" v-model="formItem.date"></DatePicker>-->
-<!--          </Col>-->
-<!--          <Col span="2" style="text-align: center">-</Col>-->
-<!--          <Col span="11">-->
-<!--            <TimePicker type="time" placeholder="Select time" v-model="formItem.time"></TimePicker>-->
-<!--          </Col>-->
-<!--        </Row>-->
-<!--      </FormItem>-->
-<!--      <FormItem label="Radio">-->
-<!--        <RadioGroup v-model="formItem.radio">-->
-<!--          <Radio label="male">Male</Radio>-->
-<!--          <Radio label="female">Female</Radio>-->
-<!--        </RadioGroup>-->
-<!--      </FormItem>-->
-      <FormItem label="菜品">
-        <CheckboxGroup v-model="formItem.checkbox">
-          <Checkbox label="西式"/>
-          <Checkbox label="中式"/>
-        </CheckboxGroup>
-      </FormItem>
-<!--      <FormItem label="Switch">-->
-<!--        <i-switch v-model="formItem.switch" size="large">-->
-<!--          <span slot="open">On</span>-->
-<!--          <span slot="close">Off</span>-->
-<!--        </i-switch>-->
-<!--      </FormItem>-->
-<!--      <FormItem label="Slider">-->
-<!--        <Slider v-model="formItem.slider" range></Slider>-->
-<!--      </FormItem>-->
       <FormItem label="Text">
-        <Input v-model="formItem.textarea" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."/>
+        <Input v-model="formItem.content" type="textarea" :autosize="{minRows: 2,maxRows: 5}"
+               placeholder="Enter something..."/>
       </FormItem>
+      <form-item>
+        <input type="file" accept="image/*" @change="upload_image($event)"/>
+      </form-item>
       <FormItem>
-        <Button type="primary">发布</Button>
+        <Button type="primary" v-on:click="upload">发布</Button>
         <Button style="margin-left: 8px" v-on:click="go_back">返回</Button>
       </FormItem>
     </Form>
@@ -56,33 +21,66 @@
 </template>
 
 <script>
-    export default {
-        name: "write_artical",
-      data () {
-        return {
-          formItem: {
-            input: '',
-            select: '',
-            radio: 'male',
-            checkbox: [],
-            switch: true,
-            date: '',
-            time: '',
-            slider: [20, 50],
-            textarea: ''
-          }
-        }
-      },methods:{
-          go_back:function () {
-            this.$router.push('/');
-          }
-      }
+  let formData = new FormData;
+  let URL_ROOT = {
+    'base_url': 'http://localhost:8080/',
+  };
+  import axios from 'axios';
 
+  export default {
+    name: "write_artical",
+    data() {
+      return {
+        formItem: {
+          content: '',
+          imageId: '',
+          title: '',
+          userId: ''
+        },
+        image_data: null
+      }
+    }, methods: {
+      go_back: function () {
+        this.$router.push('/');
+      },
+      upload: function () {
+        axios.get(URL_ROOT.base_url + 'user/me').then(res => {
+          this.formItem.userId = res.data.data.id;
+        });
+        axios.post(URL_ROOT.base_url + 'uploader', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+
+
+          }
+        }).then(res => {
+
+          this.formItem.imageId = res.data.data.id;
+          axios.post(URL_ROOT.base_url + 'article',this.formItem).then(res => {
+            alert('上传成功')
+
+
+          }).catch(error => {
+            console.log(error)
+
+
+          })
+        }).catch(error => {
+
+        })
+      },
+      upload_image: function (event) {
+        event.preventDefault();
+        formData.append('file', this.image_data);
+        this.image_data = event.target.files[0];
+
+      }
     }
+  }
 </script>
 
 <style scoped>
-  .input_artical{
+  .input_artical {
     padding-top: 8%;
     padding-left: 25%;
     padding-right: 25%;
